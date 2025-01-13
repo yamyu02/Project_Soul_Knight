a39ce859f5f3eb2a8746e6e9c3deccb7df2da729
TheMobiL <124905245+TheMobiL@users.noreply.github.com>
2025-01-13 12:39:46 -0500

    finalized* preferences screen

--- Assets/Scripts/Preferences/UpdatePref.cs
+++ Assets/Scripts/Preferences/UpdatePref.cs
@@ -8,6 +8,7 @@ public class UpdatePref : MonoBehaviour
     public Button EasyButton;
     public Button NormalButton;
     public Button HardButton;
+    public Button ResetButton;
 
     // a tiny bit of the code for the sliders was from https://johnleonardfrench.music/the-right-way-to-make-a-volume-slider-in-unity-using-logarithmic-conversion/
     public Slider MusicSlider;
@@ -19,22 +20,25 @@ public class UpdatePref : MonoBehaviour
         // LoadPrefs();
         int Difficulty = PlayerPrefs.GetInt("Difficulty", 1); // 0 = easy, 1 = normal, 2 = hard
         MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        SoundSlider.value = PlayerPrefs.GetFloat("SoundVolume", 0.5f);
 
         EasyButton.onClick.AddListener(() => TaskOnClick(0)); // no idea why i need an arrow for this to work
         NormalButton.onClick.AddListener(() => TaskOnClick(1));
         HardButton.onClick.AddListener(() => TaskOnClick(2));
+        ResetButton.onClick.AddListener(() => ResetPrefs());
 
         Debug.Log($"current difficulty: {PlayerPrefs.GetInt("Difficulty")}");
         Debug.Log($"current music volume: {PlayerPrefs.GetFloat("MusicVolume")}");
-        //Debug.Log($"current sound volume: {PlayerPrefs.GetFloat("SoundVolume")}");
+        Debug.Log($"current sound volume: {PlayerPrefs.GetFloat("SoundVolume")}");
     }
 
     // Update is called once per frame
     void Update()
     {
         SetMusicSliderLevel(MusicSlider.value);
+        SetSoundSliderLevel(SoundSlider.value);
         // Debug.Log(MusicSlider.value);
-        // Debug.Log(PlayerPrefs.GetFloat("MusicSlider"));
+        // Debug.Log(SoundSlider.value);
     }
 
     public void TaskOnClick(int difficulty)
@@ -42,22 +46,22 @@ public class UpdatePref : MonoBehaviour
         PlayerPrefs.SetInt("Difficulty", difficulty);
         PlayerPrefs.Save();
         Debug.Log($"difficulty set to {difficulty}");
-    } 
+    }
+
+    public void ResetPrefs()
+    {
+        MusicSlider.value = 0.5f;
+        SoundSlider.value = 0.5f;
+        Debug.Log("preferences reset");
+    }
 
     public void SetMusicSliderLevel(float value)
     {
         PlayerPrefs.SetFloat("MusicVolume", value);
     }
 
-    //public void SavePrefs()
-    //{
-
-    //}
-
-    //public void LoadPrefs()
-    //{
-    //    int Difficulty = PlayerPrefs.GetInt("Difficulty", 1); // 0 = easy, 1 = normal, 2 = hard
-    //    float MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f);
-    //    float SoundVolume = PlayerPrefs.GetFloat("SoundVolume", 1f);
-    //}
+    public void SetSoundSliderLevel(float value)
+    {
+        PlayerPrefs.SetFloat("SoundVolume", value);
+    }
 }
