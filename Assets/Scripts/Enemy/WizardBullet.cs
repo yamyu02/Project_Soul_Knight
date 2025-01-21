69ceca21d157d31a61fca6a06b0ac172b24bf010
TheMobiL <124905245+TheMobiL@users.noreply.github.com>
2025-01-21 13:09:21 -0500

    made bullet movement work somewhat*
    the collision is still broken but i made the movement work properly

--- Assets/Scripts/Enemy/WizardBullet.cs
+++ Assets/Scripts/Enemy/WizardBullet.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WizardBullet : MonoBehaviour
 {
+    public GameObject Sprite;
+    public float MoveSpeed;
+    public float StartingXDirection;
+    public float StartingYDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Rotate(StartingXDirection, StartingYDirection, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * -2);
+        transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+        Sprite.transform.Rotate(Vector3.forward * -1);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("wall collide");
+            Destroy(coll.gameObject);
+        }
     }
 }
